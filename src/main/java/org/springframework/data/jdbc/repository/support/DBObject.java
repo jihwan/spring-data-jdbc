38d9f7d848d08288f7a92016a5ddd71ff4258a68
zhwan <zhwan.hwang@gmail.com>
2016-05-20 17:28:13 +0900

    mapRow function : spring-data-mongodb 기능 구현을 참조하여 구현 조치

---
+++ src/main/java/org/springframework/data/jdbc/repository/support/DBObject.java
@@ -0,0 +1,12 @@
+package org.springframework.data.jdbc.repository.support;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+public class DBObject extends HashMap<Field, Object> {
+
+	private static final long serialVersionUID = -4372854073573972995L;
+
+	public DBObject() {}
+	
+}
--- src/main/java/org/springframework/data/jdbc/repository/support/JdbcBeanPropertyMapper.java
+++ src/main/java/org/springframework/data/jdbc/repository/support/JdbcBeanPropertyMapper.java
@@ -7,21 +7,32 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.PropertyAccessorFactory;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.data.convert.EntityInstantiators;
 import org.springframework.data.jdbc.mapping.JdbcMappingContext;
 import org.springframework.data.jdbc.mapping.JdbcPersistentEntity;
 import org.springframework.data.jdbc.mapping.JdbcPersistentEntityImpl;
 import org.springframework.data.jdbc.mapping.JdbcPersistentProperty;
-import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.mapping.Association;
+import org.springframework.data.mapping.AssociationHandler;
+import org.springframework.data.mapping.PersistentPropertyAccessor;
+import org.springframework.data.mapping.PropertyHandler;
+import org.springframework.data.mapping.model.ConvertingPropertyAccessor;
+import org.springframework.data.mapping.model.MappingException;
 import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.util.Assert;
 
-public class JdbcBeanPropertyMapper<T> implements BeanPropertyMapper<T> {
-
-	protected Class<T> mappedClass;
+class JdbcBeanPropertyMapper<T> implements BeanPropertyMapper<T> {
 	
+	protected static final Logger LOGGER = LoggerFactory.getLogger(JdbcBeanPropertyMapper.class);
+
+	protected GenericConversionService conversionService = new GenericConversionService();
+	protected EntityInstantiators instantiators = new EntityInstantiators();
+	
+	protected Class<T> mappedClass;
 	protected JdbcMappingContext jdbcMappingContext;
 	
 	private Map<String, JdbcPersistentProperty> mappedFields;
@@ -88,60 +99,113 @@ public class JdbcBeanPropertyMapper<T> implements BeanPropertyMapper<T> {
 			if (pd.getWriteMethod() != null && associationProperty != null) {
 				JdbcPersistentProperty persistentProperty = 
 						persistentEntity.getPersistentProperty(pd.getName());
-				
-				/**
-				 * problem ... bad code...
-				 */
-				persistentProperty.setOwnerFieldName(associationProperty.getName());
-				
 				this.mappedFields.put(pd.getName().toLowerCase(), persistentProperty);
 			}
 		}
 	}
 	
-	@Override
 	public T mapRow(ResultSet rs, int rowNum) throws SQLException {
-		
-		Assert.state(this.mappedClass != null, "Mapped class was not specified");
-		T mappedObject = BeanUtils.instantiate(this.mappedClass);
-		BeanWrapper bw = PropertyAccessorFactory.forBeanPropertyAccess(mappedObject);
+
+		@SuppressWarnings("unchecked")
+		JdbcPersistentEntity<T> persistentEntity = (JdbcPersistentEntity<T>) jdbcMappingContext.getPersistentEntity(mappedClass);
+		if (persistentEntity == null) {
+			throw new MappingException("No mapping metadata found for " + mappedClass.getName());
+		}
 		
 		ResultSetMetaData rsmd = rs.getMetaData();
 		int columnCount = rsmd.getColumnCount();
 		
+		DBObject dbObject = new DBObject();
 		
-		Map<Class<?>, BeanWrapper> assoMap = new HashMap<Class<?>, BeanWrapper>();
-
 		for (int index = 1; index <= columnCount; index++) {
-			
+
 			String column = JdbcUtils.lookupColumnName(rsmd, index);
 			JdbcPersistentProperty jdbcPersistentProperty = this.mappedFields.get(column.toLowerCase());
 			if(jdbcPersistentProperty == null) {
 				continue;
 			}
 			
-			Object columnValue = getColumnValue(rs, index, jdbcPersistentProperty);
-			
-			PersistentEntity<?, JdbcPersistentProperty> ownerEntity = jdbcPersistentProperty.getOwner();
-			if (ownerEntity.getType() == getMappedClass()) {
-				bw.setPropertyValue(jdbcPersistentProperty.getName(), columnValue);
-			}
-			else {
-				BeanWrapper associationBw;
-				if(assoMap.containsKey(ownerEntity.getType())) {
-					associationBw = assoMap.get(ownerEntity.getType());
-				}
-				else {
-					associationBw = PropertyAccessorFactory.forBeanPropertyAccess(BeanUtils.instantiate(ownerEntity.getType()));
-					assoMap.put(ownerEntity.getType(), associationBw);
-				}
-				associationBw.setPropertyValue(jdbcPersistentProperty.getName(), columnValue);
-				bw.setPropertyValue(jdbcPersistentProperty.getOwnerFieldName(), associationBw.getWrappedInstance());
-			}
+			Object columnValue = getColumnValue(rs, index, jdbcPersistentProperty);			
+			dbObject.put(jdbcPersistentProperty.getField(), columnValue);
 		}
 		
-		return mappedObject;
+		return read(persistentEntity, dbObject);
 	}
+	
+	protected T read(final JdbcPersistentEntity<?> entity, final DBObject dbObject) {
+		
+		@SuppressWarnings("unchecked")
+		T instance = (T) BeanUtils.instantiate(entity.getType());
+		
+		final PersistentPropertyAccessor accessor = 
+				new ConvertingPropertyAccessor(entity.getPropertyAccessor(instance), conversionService);
+		
+		entity.doWithProperties(new PropertyHandler<JdbcPersistentProperty>() {
+			@Override
+			public void doWithPersistentProperty(JdbcPersistentProperty persistentProperty) {
+				
+				Object object = dbObject.get(persistentProperty.getField());
+				accessor.setProperty(persistentProperty, object);
+			}
+		});
+		
+		entity.doWithAssociations(new AssociationHandler<JdbcPersistentProperty>() {
+			@Override
+			public void doWithAssociation(Association<JdbcPersistentProperty> association) {
+				
+				JdbcPersistentProperty property = association.getInverse();
+				JdbcPersistentEntity<?> acturalEntity = jdbcMappingContext.getPersistentEntity(property.getActualType());
+				T object = read(acturalEntity, dbObject);
+				accessor.setProperty(property, object);
+			}
+		});
+		
+		return instance;
+	}
+	
+//	@Override
+//	public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+//		
+//		Assert.state(this.mappedClass != null, "Mapped class was not specified");
+//		T mappedObject = BeanUtils.instantiate(this.mappedClass);
+//		BeanWrapper bw = PropertyAccessorFactory.forBeanPropertyAccess(mappedObject);
+//		
+//		ResultSetMetaData rsmd = rs.getMetaData();
+//		int columnCount = rsmd.getColumnCount();
+//		
+//		
+//		Map<Class<?>, BeanWrapper> assoMap = new HashMap<Class<?>, BeanWrapper>();
+//
+//		for (int index = 1; index <= columnCount; index++) {
+//			
+//			String column = JdbcUtils.lookupColumnName(rsmd, index);
+//			JdbcPersistentProperty jdbcPersistentProperty = this.mappedFields.get(column.toLowerCase());
+//			if(jdbcPersistentProperty == null) {
+//				continue;
+//			}
+//			
+//			Object columnValue = getColumnValue(rs, index, jdbcPersistentProperty);
+//			
+//			PersistentEntity<?, JdbcPersistentProperty> ownerEntity = jdbcPersistentProperty.getOwner();
+//			if (ownerEntity.getType() == getMappedClass()) {
+//				bw.setPropertyValue(jdbcPersistentProperty.getName(), columnValue);
+//			}
+//			else {
+//				BeanWrapper associationBw;
+//				if(assoMap.containsKey(ownerEntity.getType())) {
+//					associationBw = assoMap.get(ownerEntity.getType());
+//				}
+//				else {
+//					associationBw = PropertyAccessorFactory.forBeanPropertyAccess(BeanUtils.instantiate(ownerEntity.getType()));
+//					assoMap.put(ownerEntity.getType(), associationBw);
+//				}
+//				associationBw.setPropertyValue(jdbcPersistentProperty.getName(), columnValue);
+////				bw.setPropertyValue(jdbcPersistentProperty.getOwnerFieldName(), associationBw.getWrappedInstance());
+//			}
+//		}
+//		
+//		return mappedObject;
+//	}
 
 	@Override
 	public Map<String, Object> toMap(T entity) {
@@ -152,7 +216,7 @@ public class JdbcBeanPropertyMapper<T> implements BeanPropertyMapper<T> {
 	protected Object getColumnValue(ResultSet rs, int index, JdbcPersistentProperty entity) throws SQLException {
 		return JdbcUtils.getResultSetValue(rs, index, entity.getType());
 	}
-
+	
 	public static <T> JdbcBeanPropertyMapper<T> newInstance(Class<T> mappedClass, JdbcMappingContext jdbcMappingContext) {
 		JdbcBeanPropertyMapper<T> newInstance = new JdbcBeanPropertyMapper<T>();
 		newInstance.setMappedClass(mappedClass);
