0f426ef77957d25d358d81abf6d86d925dafad23
zhwan <zhwan.hwang@gmail.com>
2016-05-23 14:38:31 +0900

    JdbcEntityInformation 관리 기능 추가

--- src/main/java/org/springframework/data/jdbc/mapping/JdbcMappingContext.java
+++ src/main/java/org/springframework/data/jdbc/mapping/JdbcMappingContext.java
@@ -1,15 +1,26 @@
 package org.springframework.data.jdbc.mapping;
+
 import java.beans.PropertyDescriptor;
+import java.io.Serializable;
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+import org.springframework.data.jdbc.repository.support.JdbcEntityInformation;
 import org.springframework.data.mapping.context.AbstractMappingContext;
 import org.springframework.data.mapping.model.SimpleTypeHolder;
 import org.springframework.data.util.TypeInformation;
+import org.springframework.util.Assert;
 
 public class JdbcMappingContext extends
 		AbstractMappingContext<JdbcPersistentEntityImpl<?>, JdbcPersistentProperty> {
 
+	/**
+	 * root entity 정보를 관리한다.
+	 */
+	Map<Class<?>, JdbcEntityInformation<?, Serializable>> map = new HashMap<Class<?>, JdbcEntityInformation<?, Serializable>>();
+	
 	Set<Class<?>> entitySet;
 	
 	public JdbcMappingContext(Set<Class<?>> initialEntitySet) {
@@ -29,5 +40,21 @@ public class JdbcMappingContext extends
 		
 		JdbcPersistentPropertyImpl jdbcPersistentPropertyImpl = new JdbcPersistentPropertyImpl(field, descriptor, owner, simpleTypeHolder);
 		return jdbcPersistentPropertyImpl;
+	}
+
+	public void addEntityInformation(Class<?> domainClass, JdbcEntityInformation<?, Serializable> information) {
+		Assert.notNull(domainClass);
+		Assert.notNull(information);
+		map.put(domainClass, information);
+	}
+	
+	public JdbcEntityInformation<?, Serializable> getEntityInformation(Class<?> domainClass) {
+		Assert.notNull(domainClass);
+		
+		if (map.containsKey(domainClass)) {
+			return map.get(domainClass);
+		}
+		
+		throw new IllegalArgumentException(domainClass + " is not support.");
 	}
 }
