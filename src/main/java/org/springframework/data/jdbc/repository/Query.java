9523347a416b04b3f5b702b4c73f2fef7fc2ed26
zhwan <zhwan.hwang@gmail.com>
2016-05-23 17:22:24 +0900

    edit

--- src/main/java/org/springframework/data/jdbc/repository/Query.java
+++ src/main/java/org/springframework/data/jdbc/repository/Query.java
@@ -1,4 +1,5 @@
 package org.springframework.data.jdbc.repository;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,7 +10,7 @@ import org.springframework.data.annotation.QueryAnnotation;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @QueryAnnotation
-//@Documented
+@Documented
 public @interface Query {
 
 	/**
