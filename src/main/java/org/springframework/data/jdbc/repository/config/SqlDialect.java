bb5eefa035bee595b15cde5084788cd50f442dd2
zhwan <zhwan.hwang@gmail.com>
2016-05-23 14:40:42 +0900

    jdbcSqlDialect 기능 추가

---
+++ src/main/java/org/springframework/data/jdbc/repository/config/SqlDialect.java
@@ -0,0 +1,6 @@
+package org.springframework.data.jdbc.repository.config;
+
+public interface SqlDialect {
+
+	String SimpleJdbcSqlDialect = "org.springframework.data.jdbc.repository.query.SimpleJdbcSqlDialect";
+}
