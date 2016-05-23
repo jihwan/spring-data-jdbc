bec38a9a3c6af847f6701a6b47ce44a06700042f
zhwan <zhwan@gmail.com>
2016-05-23 22:40:35 +0900

    use SqlGenerator and auto register

--- src/main/java/org/springframework/data/jdbc/repository/config/SqlDialect.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.data.jdbc.repository.config;
-
-public interface SqlDialect {
-
-	String SimpleJdbcSqlDialect = "org.springframework.data.jdbc.repository.query.SimpleJdbcSqlDialect";
-}
