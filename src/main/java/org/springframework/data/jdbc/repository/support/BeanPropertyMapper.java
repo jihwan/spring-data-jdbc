a82cbd93b5994a597f6b575937ee44ff5e92e811
zhwan <zhwan.hwang@gmail.com>
2016-05-24 16:02:25 +0900

    add comment

--- src/main/java/org/springframework/data/jdbc/repository/support/BeanPropertyMapper.java
+++ src/main/java/org/springframework/data/jdbc/repository/support/BeanPropertyMapper.java
@@ -1,10 +1,24 @@
 package org.springframework.data.jdbc.repository.support;
 
+import java.io.Serializable;
 import java.util.Map;
 
 import org.springframework.jdbc.core.RowMapper;
 
-public interface BeanPropertyMapper<T> extends RowMapper<T> {
-
+/**
+ * db 2 entity. entity 2 map
+ * 
+ * @author Jihwan Hwang
+ *
+ * @param <T>
+ */
+public interface BeanPropertyMapper<T> extends RowMapper<T>, Serializable {
+	
+	/**
+	 * insert, update 용도. entity field 값이 null일 경우는 map 에 담지 않는다. 
+	 * 
+	 * @param entity
+	 * @return
+	 */
 	Map<String, Object> toMap(T entity);
 }
