3417553fa6e21ff70a83b2eb90b8e63434cd8774
zhwan <zhwan.hwang@gmail.com>
2016-05-24 15:59:42 +0900

    modify getBasePackages function

--- src/main/java/org/springframework/data/jdbc/repository/config/JdbcMappingContextFactoryBean.java
+++ src/main/java/org/springframework/data/jdbc/repository/config/JdbcMappingContextFactoryBean.java
@@ -12,23 +12,38 @@ import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.type.StandardAnnotationMetadata;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.data.annotation.Persistent;
 import org.springframework.data.jdbc.domain.JdbcPersistable;
 import org.springframework.data.jdbc.mapping.JdbcMappingContext;
-import org.springframework.data.repository.config.AnnotationRepositoryConfigurationSource;
+import org.springframework.data.repository.config.RepositoryConfigurationSource;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 
+/**
+ * {@link JdbcPersistable}을 implementation 한 jdbc Domain entity class 목록을 찾아 등록 합니다.
+ * entity class를 찾기 위해서는, {@link EnableJdbcRepositories#basePackageClasses()} 나, {@link EnableJdbcRepositories#basePackages()} 에 명시 해야 합니다.
+ * 
+ * @author Jihwan Hwang
+ *
+ */
 class JdbcMappingContextFactoryBean 
 	extends AbstractFactoryBean<JdbcMappingContext> 
 	implements ApplicationContextAware, ResourceLoaderAware {
-
-	GenericApplicationContext applicationContext;
 	
-	ResourceLoader resourceLoader;
+	/**
+	 * {@link JdbcRepositoryConfigExtension#registerBeansForRoot(org.springframework.beans.factory.support.BeanDefinitionRegistry, RepositoryConfigurationSource)}
+	 * 에서 bean property set을 한다.
+	 * 
+	 * @see JdbcRepositoryConfigExtension
+	 * @see EnableJdbcRepositories
+	 */
+	private RepositoryConfigurationSource configurationSource;
+
+	private GenericApplicationContext applicationContext;
+	
+	private ResourceLoader resourceLoader;
 	
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -38,6 +53,10 @@ class JdbcMappingContextFactoryBean
 	@Override
 	public void setResourceLoader(ResourceLoader resourceLoader) {
 		this.resourceLoader = resourceLoader;
+	}
+	
+	public void setConfigurationSource(RepositoryConfigurationSource configurationSource) {
+		this.configurationSource = configurationSource;
 	}
 	
 	@Override
@@ -50,7 +69,7 @@ class JdbcMappingContextFactoryBean
 		
 		Set<Class<?>> initialEntitySet = getInitialEntitySet();
 		
-		JdbcMappingContext jdbcMappingContext = new JdbcMappingContext(initialEntitySet);
+		JdbcMappingContext jdbcMappingContext = new JdbcMappingContext();
 		jdbcMappingContext.setInitialEntitySet(initialEntitySet);
 		jdbcMappingContext.initialize();
 		
@@ -66,18 +85,17 @@ class JdbcMappingContextFactoryBean
 	private Set<Class<?>> getInitialEntitySet() throws ClassNotFoundException {
 		
 		Set<Class<?>> entitySet = new HashSet<Class<?>>();
-		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(
-				false);
+		ClassPathScanningCandidateComponentProvider scanner = 
+				new ClassPathScanningCandidateComponentProvider(false);
 		scanner.setEnvironment(applicationContext.getEnvironment());
 		scanner.setResourceLoader(this.resourceLoader);
 		scanner.addIncludeFilter(new AssignableTypeFilter(JdbcPersistable.class));
 		scanner.addIncludeFilter(new AnnotationTypeFilter(Persistent.class));
 		for (String basePackage : getBasePackages()) {
 			if (StringUtils.hasText(basePackage)) {
-				for (BeanDefinition candidate : scanner
-						.findCandidateComponents(basePackage)) {
-					entitySet.add(ClassUtils.forName(candidate.getBeanClassName(),
-							applicationContext.getClassLoader()));
+				for (BeanDefinition candidate : scanner.findCandidateComponents(basePackage)) {
+					entitySet.add(
+							ClassUtils.forName(candidate.getBeanClassName(), applicationContext.getClassLoader()));
 				}
 			}
 		}
@@ -87,13 +105,6 @@ class JdbcMappingContextFactoryBean
 	
 	private Iterable<String> getBasePackages() {
 
-		BeanDefinition beanDefinition = applicationContext.getBeanDefinition(JdbcRepositoryConfigExtension.JDBC_MAPPING_CONTEXT_BEAN_NAME);
-		Object source = beanDefinition.getSource();
-		StandardAnnotationMetadata metadata = (StandardAnnotationMetadata)source;
-		
-		AnnotationRepositoryConfigurationSource annotationRepositoryConfigurationSource = 
-				new AnnotationRepositoryConfigurationSource(metadata, EnableJdbcRepositories.class, resourceLoader, applicationContext.getEnvironment());
-		
-		return annotationRepositoryConfigurationSource.getBasePackages();
+		return configurationSource.getBasePackages();
 	}
 }
