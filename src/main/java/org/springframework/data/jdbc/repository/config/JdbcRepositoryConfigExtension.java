21d8cb04af42dd9d89a59de87b85b1726bd4dce3
zhwan <zhwan.hwang@gmail.com>
2016-05-24 15:59:00 +0900

    edit registerBeanForRoot function : JdbcMappingContextFactoryBean add property

--- src/main/java/org/springframework/data/jdbc/repository/config/JdbcRepositoryConfigExtension.java
+++ src/main/java/org/springframework/data/jdbc/repository/config/JdbcRepositoryConfigExtension.java
@@ -2,14 +2,8 @@ package org.springframework.data.jdbc.repository.config;
 
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
-//import java.lang.annotation.Annotation;
-//import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Locale;
-
-//import javax.persistence.Entity;
-//import javax.persistence.MappedSuperclass;
 
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -24,10 +18,15 @@ import org.springframework.data.repository.config.RepositoryConfigurationSource;
 import org.springframework.data.repository.config.XmlRepositoryConfigurationSource;
 import org.springframework.util.StringUtils;
 
+/**
+ * 여기서 overriding 하는 함수의 기능은 {@link JdbcRepositoryFactoryBean} 을 setting 하기 위한,
+ * bean 생성이나, 필요한 bean을 가져 오기 위한 역할을 한다.
+ * 
+ * @author Jihwan Hwang
+ *
+ */
 public class JdbcRepositoryConfigExtension extends RepositoryConfigurationExtensionSupport {
 
-	// ================================= 주석 처리
-//	private static final Class<?> PAB_POST_PROCESSOR = PersistenceAnnotationBeanPostProcessor.class;
 	private static final String DEFAULT_TRANSACTION_MANAGER_BEAN_NAME = "transactionManager";
 	private static final String DEFAULT_JDBC_TEMPLATE_BEAN_NAME = "jdbcTemplate";
 	private static final String ENABLE_DEFAULT_TRANSACTIONS_ATTRIBUTE = "enableDefaultTransactions";
@@ -46,7 +45,7 @@ public class JdbcRepositoryConfigExtension extends RepositoryConfigurationExtens
 
 	@Override
 	protected String getModulePrefix() {
-		return getModuleName().toLowerCase(Locale.US);
+		return getModuleName().toLowerCase();
 	}
 
 	@Override
@@ -65,19 +64,18 @@ public class JdbcRepositoryConfigExtension extends RepositoryConfigurationExtens
 		String transactionManagerRef = source.getAttribute("transactionManagerRef");
 		builder.addPropertyValue("transactionManager",
 				transactionManagerRef == null ? DEFAULT_TRANSACTION_MANAGER_BEAN_NAME : transactionManagerRef);
-		builder.addPropertyReference("mappingContext", JDBC_MAPPING_CONTEXT_BEAN_NAME);
-		
 		
 		String jdbcTemplateRef = source.getAttribute("jdbcTemplateRef");
 		builder.addPropertyValue("jdbcTemplate",
 				jdbcTemplateRef == null ? DEFAULT_JDBC_TEMPLATE_BEAN_NAME : jdbcTemplateRef);
+		
+		builder.addPropertyReference("mappingContext", JDBC_MAPPING_CONTEXT_BEAN_NAME);
 	}
 
 	@Override
 	public void postProcess(BeanDefinitionBuilder builder, AnnotationRepositoryConfigurationSource config) {
 
 		AnnotationAttributes attributes = config.getAttributes();
-
 		builder.addPropertyValue(ENABLE_DEFAULT_TRANSACTIONS_ATTRIBUTE,
 				attributes.getBoolean(ENABLE_DEFAULT_TRANSACTIONS_ATTRIBUTE));
 	}
@@ -86,7 +84,6 @@ public class JdbcRepositoryConfigExtension extends RepositoryConfigurationExtens
 	public void postProcess(BeanDefinitionBuilder builder, XmlRepositoryConfigurationSource config) {
 
 		String enableDefaultTransactions = config.getAttribute(ENABLE_DEFAULT_TRANSACTIONS_ATTRIBUTE);
-
 		if (StringUtils.hasText(enableDefaultTransactions)) {
 			builder.addPropertyValue(ENABLE_DEFAULT_TRANSACTIONS_ATTRIBUTE, enableDefaultTransactions);
 		}
@@ -97,65 +94,10 @@ public class JdbcRepositoryConfigExtension extends RepositoryConfigurationExtens
 		
 		super.registerBeansForRoot(registry, config);
 		Object source = config.getSource();
-				
-		registerIfNotAlreadyRegistered(new RootBeanDefinition(JdbcMappingContextFactoryBean.class), registry, JDBC_MAPPING_CONTEXT_BEAN_NAME, source);
+		
+		RootBeanDefinition rootBeanDefinition = new RootBeanDefinition(JdbcMappingContextFactoryBean.class);
+		// use for : scanning custom entity class
+		rootBeanDefinition.getPropertyValues().add("configurationSource", config);
+		registerIfNotAlreadyRegistered(rootBeanDefinition, registry, JDBC_MAPPING_CONTEXT_BEAN_NAME, source);
 	}
-	
-	
-	// ================================= 주석 처리
-//	/* 
-//	 * (non-Javadoc)
-//	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#registerBeansForRoot(org.springframework.beans.factory.support.BeanDefinitionRegistry, org.springframework.data.repository.config.RepositoryConfigurationSource)
-//	 */
-//	@Override
-//	public void registerBeansForRoot(BeanDefinitionRegistry registry, RepositoryConfigurationSource config) {
-//
-//		super.registerBeansForRoot(registry, config);
-//
-//		Object source = config.getSource();
-//
-//		registerIfNotAlreadyRegistered(new RootBeanDefinition(EntityManagerBeanDefinitionRegistrarPostProcessor.class),
-//				registry, EM_BEAN_DEFINITION_REGISTRAR_POST_PROCESSOR_BEAN_NAME, source);
-//
-//		registerIfNotAlreadyRegistered(new RootBeanDefinition(JpaMetamodelMappingContextFactoryBean.class), registry,
-//				JPA_MAPPING_CONTEXT_BEAN_NAME, source);
-//
-//		registerIfNotAlreadyRegistered(new RootBeanDefinition(PAB_POST_PROCESSOR), registry,
-//				AnnotationConfigUtils.PERSISTENCE_ANNOTATION_PROCESSOR_BEAN_NAME, source);
-//
-//		// Register bean definition for DefaultJpaContext
-//
-//		RootBeanDefinition contextDefinition = new RootBeanDefinition(DefaultJpaContext.class);
-//		contextDefinition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_CONSTRUCTOR);
-//
-//		registerIfNotAlreadyRegistered(contextDefinition, registry, JPA_CONTEXT_BEAN_NAME, source);
-//	}
-
-//	/**
-//	 * Creates an anonymous factory to extract the actual {@link javax.persistence.EntityManager} from the
-//	 * {@link javax.persistence.EntityManagerFactory} bean name reference.
-//	 * 
-//	 * @param entityManagerFactoryBeanName
-//	 * @param source
-//	 * @return
-//	 */
-//	private static AbstractBeanDefinition getEntityManagerBeanDefinitionFor(RepositoryConfigurationSource config,
-//			Object source) {
-//
-//		BeanDefinitionBuilder builder = BeanDefinitionBuilder
-//				.rootBeanDefinition("org.springframework.orm.jpa.SharedEntityManagerCreator");
-//		builder.setFactoryMethod("createSharedEntityManager");
-//		builder.addConstructorArgReference(getEntityManagerBeanRef(config));
-//
-//		AbstractBeanDefinition bean = builder.getRawBeanDefinition();
-//		bean.setSource(source);
-//
-//		return bean;
-//	}
-//
-//	private static String getEntityManagerBeanRef(RepositoryConfigurationSource config) {
-//
-//		String entityManagerFactoryRef = config == null ? null : config.getAttribute("entityManagerFactoryRef");
-//		return entityManagerFactoryRef == null ? "entityManagerFactory" : entityManagerFactoryRef;
-//	}
 }
