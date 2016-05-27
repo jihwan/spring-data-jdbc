40d508b5df91e1701fc8403241adb8fc799cc4c1
Jihwan,Hwang <zhwan.hwang@gmail.com>
2016-05-27 10:51:31 +0900

    edit

--- src/test/java/org/springframework/data/jdbc/domain/sample/Foo.java
+++ src/test/java/org/springframework/data/jdbc/domain/sample/Foo.java
@@ -8,14 +8,16 @@ import org.springframework.data.jdbc.domain.JdbcPersistable;
 
 import lombok.Data;
 
-@Persistent
-@Data
-public class Foo
-//	extends AbstractJdbcPersistable<Foo, Bar> {
-	implements JdbcPersistable<Foo, Bar> {
+@Persistent @Data
+public class Foo implements JdbcPersistable<Foo, Bar> {
 
 	private static final long serialVersionUID = 8631471766926473778L;
 
+	private transient boolean persisted;
+	
+	@Id
+	Bar bar;
+	
 	@Reference
 	Address addR;
 	
@@ -23,51 +25,13 @@ public class Foo
 
 	public Foo() {}
 	
+	public Foo(Bar bar) {
+		this.bar = bar;
+	}
+	
 	public static Foo instance(Bar bar) {
 		Foo foo = new Foo(bar);
 		return foo;
-	}
-	
-/**
- * AbstractJdbcPersistable codes
- */	
-//	public Foo(Bar bar) {
-//		setId(bar);
-//	}
-//
-//	public Address getAddR() {
-//		return addR;
-//	}
-//	
-//	public void setAddR(Address addR) {
-//		this.addR = addR;
-//	}
-//	
-//	public String getName() {
-//		return name;
-//	}
-//	
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Foo [addr=" + addR + ", name=" + name + ", toString()=" + super.toString() + "]";
-//	}
-	
-	
-	
-/**
- * JdbcPersistable codes
- */
-	transient boolean persisted;
-	
-	@Id
-	Bar bar;
-	
-	public Foo(Bar bar) {
-		this.bar = bar;
 	}
 	
 	@Override
